f604b4ab9fc0c798d00669ead40ad6f5cb75c19e
MRSS <mateusyasd@hotmail.com>
2021-06-30 19:52:30 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export default function Home() {
+  return(
+    <div>
+     <p className="helloWorld">This website is under construction.<br />
+     <span className="desc">Stay tuned!</span></p>
+    </div>
+  )
+}
